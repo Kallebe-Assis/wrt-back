bcdef4456eaea838192a403e3b092873d4ea7248
Kallebe-Assis <kallebe.assiss@gmail.com>
2025-07-25 00:23:58 -0300

    CORS: adicionar Access-Control-Allow-Headers: Content-Type (preflight fix)

--- api/auth/login.js
+++ api/auth/login.js
@@ -4,6 +4,7 @@ module.exports = async function handler(req, res) {
   // CORS EXPLÍCITO
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
